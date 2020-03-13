46ca29d5fc19857c39ec060b445d747677f534a6
Christian <christian.roeder@reservix.de>
2020-03-13 10:13:44 +0100

    sql dump updated;

--- server/the_fridge_sql_dump.sql
+++ server/the_fridge_sql_dump.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.17  Distrib 10.4.10-MariaDB, for Linux (x86_64)
+-- MariaDB dump 10.17  Distrib 10.4.12-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: the_fridge
 -- ------------------------------------------------------
--- Server version	10.4.10-MariaDB
+-- Server version	10.4.12-MariaDB-1:10.4.12+maria~bionic
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `the_fridge`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `the_fridge` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `the_fridge`;
+
+--
 -- Table structure for table `fridges`
 --
 
@@ -27,9 +35,8 @@ CREATE TABLE `fridges` (
   `name` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
-  UNIQUE KEY `id` (`id`),
   UNIQUE KEY `fridges_unique` (`id`,`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +45,7 @@ CREATE TABLE `fridges` (
 
 LOCK TABLES `fridges` WRITE;
 /*!40000 ALTER TABLE `fridges` DISABLE KEYS */;
-INSERT INTO `fridges` VALUES (8,'MyFridge'),(11,'MyFridge2');
+INSERT INTO `fridges` VALUES (8,'MyFridge'),(40,'MyFridge2');
 /*!40000 ALTER TABLE `fridges` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,6 +63,7 @@ CREATE TABLE `products` (
   `min_amount` int(11) NOT NULL,
   `fridge_id` int(11) NOT NULL,
   `purchased` tinyint(1) NOT NULL,
+  `amount_to_buy` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`name`),
   KEY `fridge_id` (`fridge_id`),
   CONSTRAINT `products_ibfk_1` FOREIGN KEY (`fridge_id`) REFERENCES `fridges` (`id`)
@@ -68,6 +76,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES ('Butter',3,0,1,8,1,0),('Käse',3,0,0,8,0,9),('Milch',2,1,1,8,1,0),('quark',8,1,1,8,1,0),('Sahne',0,0,0,8,0,2);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +89,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-09 20:19:09
+-- Dump completed on 2020-03-13  9:11:29
