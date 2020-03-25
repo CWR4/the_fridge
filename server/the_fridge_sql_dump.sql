ad4e9fa49d582b9751aa7da9c873d1904ec42c1a
Ober, Thomas <Thomas.Ober@dvag.com>
2020-03-25 15:14:46 +0100

    [ TF_1.6 ] Primärschlüssel in Produkttabelle anpassen -> Done; Added some new data

--- server/the_fridge_sql_dump.sql
+++ server/the_fridge_sql_dump.sql
@@ -24,11 +24,10 @@ DROP TABLE IF EXISTS `fridges`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fridges` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(35) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  UNIQUE KEY `fridges_unique` (`id`,`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +36,7 @@ CREATE TABLE `fridges` (
 
 LOCK TABLES `fridges` WRITE;
 /*!40000 ALTER TABLE `fridges` DISABLE KEYS */;
-INSERT INTO `fridges` VALUES (41,'fridge10'),(8,'MyFridge'),(40,'MyFridge2'),(43,'testFridge');
+INSERT INTO `fridges` VALUES (3,'fridge1'),(4,'fridge2');
 /*!40000 ALTER TABLE `fridges` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,16 +48,18 @@ DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
-  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
   `amount` int(11) NOT NULL,
   `always_available` tinyint(1) NOT NULL,
   `min_amount` int(11) NOT NULL,
   `fridge_id` int(11) NOT NULL,
   `purchased` tinyint(1) NOT NULL,
-  `amount_to_buy` int(11) NOT NULL DEFAULT 0,
-  KEY `fridge_id` (`fridge_id`),
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`fridge_id`) REFERENCES `fridges` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `amount_to_buy` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_products_fridge_id` (`fridge_id`),
+  CONSTRAINT `fk_products_fridge_id` FOREIGN KEY (`fridge_id`) REFERENCES `fridges` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +68,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES ('Butter',3,0,1,8,1,0),('Käse',3,0,0,8,0,9),('Milch',2,1,1,8,1,0),('quark',8,1,1,8,1,0),('Sahne',0,0,0,8,0,2),('Milch',3,1,1,43,1,0),('Butter',2,1,1,43,1,0),('Gouda',0,0,0,43,0,2),('Lauch',0,0,0,43,0,3),('Salami',0,1,1,43,0,2);
+INSERT INTO `products` VALUES (1,'Milch',1,1,1,3,0,1),(4,'Butter',2,1,1,3,0,0),(5,'Butter',2,0,0,4,0,0),(6,'Salami',0,0,0,4,0,1),(7,'Gouda',0,1,1,4,0,2);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-23 15:23:05
+-- Dump completed on 2020-03-25 15:13:02
