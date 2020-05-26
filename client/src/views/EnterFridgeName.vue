f8d1a642168349ef79b1e9bd4427e5f0c5e109a5
Ober, Thomas <Thomas.Ober@dvag.com>
2020-05-26 20:35:11 +0200

    added bootswatch; added two buttons

--- client/src/views/EnterFridgeName.vue
+++ client/src/views/EnterFridgeName.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="enter-name-container">
-      <div class="fridge-circle">
-        <p id="the">the</p>
-        <h1>Fridge</h1>
-        <p id="end">The End of empty Fridges</p>
-      </div>
-  </div>
+    <div class="enter-name-container">
+        <div class="fridge-circle">
+            <p id="the">the</p>
+            <h1>Fridge</h1>
+            <p id="end">The End of empty Fridges</p>
+        </div>
+        <button type="button" class="btn btn-primary standard-fridge-button">NEW FRIDGE</button>
+        <button type="button" class="btn btn-primary standard-fridge-button">OPEN FRIDGE</button>
+    </div>
 </template>
 
 <script lang="ts">
@@ -42,12 +44,27 @@ export default class EnterFridgeName extends Vue {
     }
     #the {
         position: relative;
-        right: 98px;
-        top: 30px;
+        right: 94px;
+        top: 24px;
         font-size: 24px;
     }
     #end {
         color: rgba(255, 255, 255, 0.65)
+    }
+}
+
+.standard-fridge-button {
+    color: white;
+    background-color: #0F7195;
+    transition: .3s ease-in-out;
+    display: block;
+    margin: 10px auto;
+    min-width: 150px;
+
+    &:hover {
+        background-color: white;
+        color: #0F7195;
+        transition: .3s ease-in-out;
     }
 }
 </style>
