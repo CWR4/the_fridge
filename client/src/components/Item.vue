3b7626399cc110510b3c1c082a61c5e4f69473fe
Ober, Thomas <Thomas.Ober@dvag.com>
2020-06-01 18:59:02 +0200

    added Item component

---
+++ client/src/components/Item.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="item-container">
+      <p class="item-name">{{ name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+
+export default class Item extends Vue {
+    $props: {
+        name: string;
+        amount: number;
+        purchased: boolean;
+        amountToBuy: number;
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
